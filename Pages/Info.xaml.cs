f922ecff0bf99aa7538506b6c2e1ee98cb5ad62e
Yaroslav <Yaroslavkrupitsa@gmail.com>
2026-02-04 12:46:26 +0200

    Created and connected ViewModels to pages they belong

--- Pages/Info.xaml.cs
+++ Pages/Info.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestTaskApi.ViewModels;
 
 namespace TestTaskApi.Pages
 {
@@ -23,6 +24,8 @@ namespace TestTaskApi.Pages
 		public Info()
 		{
 			InitializeComponent();
+			InfoViewModel vm = new InfoViewModel();
+			DataContext = vm;
 		}
 	}
 }
